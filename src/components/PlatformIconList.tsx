067dd163ef6822236b4baa40fd4629f93935c492
Joseph Waldron <waldrojo@kean.edu>
2023-03-14 12:21:42 -0400

    added game cards

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,40 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons/lib/esm/iconBase";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+    ios: MdPhoneIphone,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
