02f7b994ed0973d6fc5045fefeec023854499e6d
Joseph Waldron <waldrojo@kean.edu>
2023-03-14 23:52:08 -0400

    added smaller images and rating

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,8 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 interface Props {
   game: Game;
@@ -8,12 +10,15 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}></Image>
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent={"space-between"}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic}></CriticScore>
+        </HStack>
       </CardBody>
     </Card>
   );
