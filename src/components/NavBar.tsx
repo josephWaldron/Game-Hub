3afe97cc974e3ac605d794eb4f4fe89d79ea6386
Joseph Waldron <waldrojo@kean.edu>
2023-03-16 22:52:43 -0400

    fixed images

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,10 @@
 import { HStack, Image, useColorModeValue } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
+import lightLogo from "../assets/logo_dark.png";
+import darkLogo from "../assets/logo_light.png";
 
 const NavBar = () => {
-  const logoSrc = useColorModeValue(
-    "../src/assets/logo_light.png",
-    "../src/assets/logo_dark.png"
-  );
+  const logoSrc = useColorModeValue(darkLogo, lightLogo);
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logoSrc} boxSize="100px" />
